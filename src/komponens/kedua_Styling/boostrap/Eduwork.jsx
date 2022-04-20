66d85e042171b11a32baba20ce3d3f8e1739691c
willyhp123 <limwilly0@gmail.com>
2022-04-20 22:19:29 +0700

    Update

--- src/komponens/kedua_Styling/boostrap/Eduwork.jsx
+++ src/komponens/kedua_Styling/boostrap/Eduwork.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 
+
 export default class Images extends React.Component{
        render(){
            return(
@@ -8,6 +9,7 @@ export default class Images extends React.Component{
                    <Card>
                     <Card.Header>Eduwork Reactjs</Card.Header>
                     <Card.Body>
+                    <img src="image/51.jpeg" width="100%" height="400px"  />
                     </Card.Body>
                     </Card>
                </div>
