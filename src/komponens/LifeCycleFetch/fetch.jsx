717f08aef09868de5618cf7c521b54a95b695540
willyhp123 <limwilly0@gmail.com>
2022-04-21 09:45:41 +0700

    Update Revisi LifeCycleComponent

--- src/komponens/LifeCycleFetch/fetch.jsx
+++ src/komponens/LifeCycleFetch/fetch.jsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { Card, Col, Container, Form, Row } from 'react-bootstrap';
+import { messages } from 'validatorjs/src/lang';
 
 
 export default class FetchData extends React.Component {
+  
   state = {beritas: [],search:''}
-   
   get_Data = (val) =>{
-      if(val === ''){
-        val = 'all';
-      }
-
+    if(val === ''){
+       val = 'all';
+    }
       this.setState({loading:true})
       fetch(`https://newsapi.org/v2/everything?q=${val}&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19`)
-      .then((response)=> response.json())  
+      .then((response)=>{
+          response.json()
+      })  
       .then(response => {
           const beritas = response.articles;
-          console.log({beritas})
           this.setState({ beritas });
         }) 
+        .catch(err=>{
+         return err.message;
+        })
     } 
-
+    messagers(messages){
+      return messages;
+    }
     componentDidMount(){
       const val = '';
      this.get_Data(val)
     }
-
     search = async val =>{
      this.get_Data(val);
     }
-
   onChangeHandler = async e => {
     if(this.search === ''){
       this.search('all');
@@ -39,18 +43,20 @@ export default class FetchData extends React.Component {
     }};
 
   render(){
+    
     return (
       <div>
         <Container>
         <Row>
           <h3>Tugas LifeCycle Component</h3>
-         
+    
       <Form.Group className="mb-3" controlId="">
       <Form.Control type="text" name ="search"  placeholder="Search Country" value={this.state.value} 
       onChange={e => this.onChangeHandler(e)}
       />
       </Form.Group>
-            {
+            { 
+
             this.state.beritas.map((berita,i) =>
             <Col key={i}  sm={4}>
               <Card style={{ width: '18rem' }}>
@@ -64,6 +70,10 @@ export default class FetchData extends React.Component {
                 </Card.Body>
               </Card>
               </Col>)
+            
+          }
+          {
+              console.log(this.messagers(messages))
           }
         </Row>
 </Container>
