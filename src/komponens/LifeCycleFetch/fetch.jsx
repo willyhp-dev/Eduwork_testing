7ca5ad78dd1fc6ce627a41388e9e4df8ad634cad
willyhp123 <limwilly0@gmail.com>
2022-04-22 14:36:46 +0700

    Update Revisi LifeCycle Component and Hooks

--- src/komponens/LifeCycleFetch/fetch.jsx
+++ src/komponens/LifeCycleFetch/fetch.jsx
@@ -3,25 +3,33 @@ import { Card, Col, Container, Form, Row } from 'react-bootstrap';
 import { messages } from 'validatorjs/src/lang';
 
 
+
 export default class FetchData extends React.Component {
   
-  state = {beritas: [],search:''}
+  state = {beritas: [],search:'',error:false}
   get_Data = (val) =>{
     if(val === ''){
-       val = 'all';
+          val = 'all';
     }
-      this.setState({loading:true})
-      fetch(`https://newsapi.org/v2/everything?q=${val}&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19`)
-      .then((response)=>
-          response.json()
-      )  
-      .then(response => {
-          const beritas = response.articles;
-          this.setState({ beritas });
-        }) 
-        .catch(err=>{
-          this.setState({ error: err.message } )
-        })
+    this.setState({ loading: true });
+    const data = fetch(`https://newsapi.org/v2/everything?q=all&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19`);
+    if (data){
+      data.then((response) =>
+      response.json()
+  )  
+  .then(response => {
+      const beritas = response.articles;
+      this.setState({ beritas });
+    }) 
+    .catch(err=>{
+      this.setState({ error: err.message } )
+    })
+    } 
+    else {
+      this.setState({error:true})
+    }
+  
+     
     } 
     messagers(messages){
       return messages;
@@ -55,26 +63,26 @@ export default class FetchData extends React.Component {
       onChange={e => this.onChangeHandler(e)}
       />
       </Form.Group>
-    
             { 
-              !this.state.error ?
-            this.state.beritas.map((berita,i) =>
-            <Col key={i}  sm={4}>
-              <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={berita.urlToImage} />
-                <Card.Body>
-                  <Card.Title>{berita.title}</Card.Title>
-                  <Card.Text>
-                   {berita.description}
-                  </Card.Text>
-                  <a href={berita.url} className='btn btn-primary'>Go Somewhere</a>
-                </Card.Body>
-              </Card>
-              </Col>)
+              !this.state.beritas ?
+              <div className='alert alert-danger alert-sm'>
+              Tidak Ada Response Data
+             </div>
+
               :
-            <div className='alert alert-danger alert-sm'>
-                  {this.state.error}
-            </div>
+              this.state.beritas.map((berita,i) =>
+              <Col key={i}  sm={4}>
+                <Card style={{ width: '18rem' }}>
+                  <Card.Img variant="top" src={berita.urlToImage} />
+                  <Card.Body>
+                    <Card.Title>{berita.title}</Card.Title>
+                    <Card.Text>
+                     {berita.description}
+                    </Card.Text>
+                    <a href={berita.url} className='btn btn-primary'>Go Somewhere</a>
+                  </Card.Body>
+                </Card>
+                </Col>)
           }
           {
               console.log(this.messagers(messages))
--- src/komponens/hooks/index.jsx
+++ src/komponens/hooks/index.jsx
@@ -1,139 +1,131 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import {  Card, Col, Container, Form, Row } from 'react-bootstrap';
-
-
-
-export default function FetchHooks(){
-    
-    const [beritas,setBerita] = useState([]);
-    const [searchTerm,SetsearchTerm]= useState('');
-    const [searchResult,setSearchResult] = useState([]);
-    const [error,seterror] = useState(false);
-    useEffect(() => {
-        // fetch('https://newsapi.org/v2/everything?q=all&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19')
-        // .then(response => response.json())
-        // .then(data =>setBerita([data.articles])) 
-        AxiosData()
-    }, []);   
-    const AxiosData = async()=>{
-        let Response = await axios.get("https://newsapi.org/v2/everything?q=all&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19");
-        if(!Response){
-        seterror(true);
-        }
-        else{
-            console.log(Response.data.articles);
-            setBerita(Response.data.articles);
-        }
-        // axios.get("https://newsapi.org/v2/everything?q=all&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19")
-        // .then(response =>response.json())
-        // .then(response =>setBerita(response.articles))
-        // .catch(err =>{
-        //    setError(true);
-        
-        // })
-       
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
+export default function FetchHooks() {
+  const [beritas, setBerita] = useState([]);
+  const [searchTerm, SetsearchTerm] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [error, seterror] = useState(true);
+  useEffect(() => {
+    // fetch('https://newsapi.org/v2/everything?q=all&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19')
+    // .then(response => response.json())
+    // .then(data =>setBerita([data.articles]))
+    AxiosData();
+  }, []);
+  const AxiosData = async () => {
+    let Response = await axios.get(
+      "https://newsapi.org/v2/everything?q=all&from=2022-04-15&sortBy=popularity&apiKey=9c2510131de24d47a20d9f258085ac19"
+    );
+    if (!Response) {
+      seterror(true);
+    } else {
+      seterror(false);
+      console.log(Response.data.articles);
+      setBerita(Response.data.articles);
     }
-    const SearchData =(value)=>{
-        SetsearchTerm(value);
+  };
+  const SearchData = (value) => {
+    SetsearchTerm(value);
 
-        if(searchTerm!==''){
-            const FilterData = beritas.filter((item) =>{
-                return Object.values(item).join('').toLowerCase().includes(searchTerm.toLowerCase())
-            })
-            setSearchResult(FilterData);
-        }else{
-            setSearchResult(beritas);
-        }
-       
+    if (searchTerm !== "") {
+      const FilterData = beritas.filter((item) => {
+        return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      });
+      setSearchResult(FilterData);
+    } else {
+      setSearchResult(beritas);
     }
-    // const Search = ({label,type,placeholder})=>{
-    //     return(
-    //     <Form.Group className="mb-3">
-    //         <Form.Label>{label}</Form.Label>
-    //         <Form.Control type={type} placeholder={placeholder} />
-    //     </Form.Group>
-    //     )}
-    
-    // const Cards = ({title,text,image,link})=>{
-    //     <Col sm ={4}>
-    //     <Card style={{ width: '18rem' }}>
-    //         <Card.Img variant="top" src={image} />
-    //         <Card.Body>
-    //             <Card.Title>{title}</Card.Title>
-    //             <Card.Text>
-    //             {text}
-    //             </Card.Text>
-    //             <a href ={link} className='btn btn-primary'>Go somewhere</a>
-    //         </Card.Body>
-    //     </Card> 
-    //     </Col>
-    // }
-    console.log(searchResult)
-return(
+  };
+  // const Search = ({label,type,placeholder})=>{
+  //     return(
+  //     <Form.Group className="mb-3">
+  //         <Form.Label>{label}</Form.Label>
+  //         <Form.Control type={type} placeholder={placeholder} />
+  //     </Form.Group>
+  //     )}
+
+  // const Cards = ({title,text,image,link})=>{
+  //     <Col sm ={4}>
+  //     <Card style={{ width: '18rem' }}>
+  //         <Card.Img variant="top" src={image} />
+  //         <Card.Body>
+  //             <Card.Title>{title}</Card.Title>
+  //             <Card.Text>
+  //             {text}
+  //             </Card.Text>
+  //             <a href ={link} className='btn btn-primary'>Go somewhere</a>
+  //         </Card.Body>
+  //     </Card>
+  //     </Col>
+  // }
+  console.log(searchResult);
+  return (
     <div>
-        <Container>
+      <Container>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Label>Tugas HOOKS</Form.Label>
-        <Form.Control type="text" placeholder="Search Country" onChange={(e)=>SearchData(e.target.value)} />
-    </Form.Group>
-            <Row>
-                {
-                    error && <div className='alert alert-danger'>
-                        Tidak Ada Response Data
-                    </div>
-                }
-        {
-            searchTerm.length > 1 ? (
-                searchResult.map(a=>(
-                    <Col sm ={4}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={a.urlToImage} />
-                        <Card.Body>
-                            <Card.Title>{a.title}</Card.Title>
-                            <Card.Text>
-                            {a.description}
-                            </Card.Text>
-                            <a href ={a.url} className='btn btn-primary'>Go somewhere</a>
-                        </Card.Body>
-                    </Card> 
-                    </Col>
-                    ))
-            ):(
-                beritas.map(a=>(
-                    <Col sm ={4}>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={a.urlToImage} />
-                        <Card.Body>
-                            <Card.Title>{a.title}</Card.Title>
-                            <Card.Text>
-                            {a.description}
-                            </Card.Text>
-                            <a href ={a.url} className='btn btn-primary'>Go somewhere</a>
-                        </Card.Body>
-                    </Card> 
-                    </Col>
-                    ))
-            )
-        }
+          <Form.Label>Tugas HOOKS</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Search Country"
+            onChange={(e) => SearchData(e.target.value)}
+          />
+        </Form.Group>
+        <Row>
+          {error ? (
+            <div className="alert alert-danger">Tidak Ada Response Data</div>
+          ) : searchTerm.length > 1 ? (
+            searchResult.map((a) => (
+              <Col sm={4}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={a.urlToImage} />
+                  <Card.Body>
+                    <Card.Title>{a.title}</Card.Title>
+                    <Card.Text>{a.description}</Card.Text>
+                    <a href={a.url} className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          ) : (
+            beritas.map((a) => (
+              <Col sm={4}>
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img variant="top" src={a.urlToImage} />
+                  <Card.Body>
+                    <Card.Title>{a.title}</Card.Title>
+                    <Card.Text>{a.description}</Card.Text>
+                    <a href={a.url} className="btn btn-primary">
+                      Go somewhere
+                    </a>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))
+          )}
+          {}
         </Row>
-        </Container>
+      </Container>
     </div>
-)
-// let [dogImage, setDogImage] = useState(null)
-//  // 3. Create out useEffect function
-//  useEffect(() => {
-     
-//     fetch("https://dog.ceo/api/breeds/image/random")
-//     .then(response => response.json())
-//         // 4. Setting *dogImage* to the image url that we received from the response above
-//     .then(data => setDogImage(data.message))
-//   },[])
+  );
+  // let [dogImage, setDogImage] = useState(null)
+  //  // 3. Create out useEffect function
+  //  useEffect(() => {
 
-//   return (
-//     <div className="App">
-//         {/* 5. Using *dogImage as* the *src* for our image*/}
-//     {dogImage && <img src={dogImage}></img>}
-//     </div>
-//   );
+  //     fetch("https://dog.ceo/api/breeds/image/random")
+  //     .then(response => response.json())
+  //         // 4. Setting *dogImage* to the image url that we received from the response above
+  //     .then(data => setDogImage(data.message))
+  //   },[])
+
+  //   return (
+  //     <div className="App">
+  //         {/* 5. Using *dogImage as* the *src* for our image*/}
+  //     {dogImage && <img src={dogImage}></img>}
+  //     </div>
+  //   );
 }
